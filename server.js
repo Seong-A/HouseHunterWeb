1301cd66400c250ca20f1d8111cf8e265e0ec98f
Eunhye <grace32612@naver.com>
2024-03-26 12:08:57 +0900

    main

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.listen(5500, function(){
     console.log('listening on 5500');
@@ -8,5 +9,10 @@ app.listen(5500, function(){
 app.get('/', (req,res) => {
     res.send('Hello World!')
 })
+
+app.get('/main', (req, res) => {
+    const filePath = path.join(__dirname, 'public', 'html', 'main.html');
+    res.sendFile(filePath);
+});
 
 app.use('/public', express.static('public'))
