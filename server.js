377f929e773ee6541857179ba4f98731e88a574b
Eunhye <grace32612@naver.com>
2024-03-28 22:23:18 +0900

    update userinfo & mypage

--- server.js
+++ server.js
@@ -69,9 +69,6 @@ app.get('/loaduser', (req, res) => {
     }
 });
 
-
-
-
 app.get('/map', (req, res) => {
     res.render('map', { naverMapAPIClientID: process.env.REACT_APP_API_CLIENT_ID });
 });
@@ -94,6 +91,10 @@ app.get('/join', (req, res) => {
 
 app.get('/login', (req, res) => {
     res.render('login');
+});
+
+app.get('/userinfo', (req, res) => {
+    res.render('userinfo');
 });
 
 app.get('/check-login', (req, res) => {
@@ -119,7 +120,6 @@ app.get('/check-login', (req, res) => {
     }
 });
 
-
 app.post('/register', (req, res) => {
     const userData = req.body;
 
@@ -133,6 +133,50 @@ app.post('/register', (req, res) => {
         } else {
             console.log('사용자 등록 성공');
             res.status(200).json({ success: true, message: '사용자 등록 성공' });
+        }
+    });
+});
+
+app.get('/current_user_info', (req, res) => {
+    if (req.session.isLoggedIn) {
+        const userId = req.session.userId;
+        const query = 'SELECT id, name, phone FROM user WHERE id = ?';
+
+        connection.query(query, [userId], (error, results, fields) => {
+            if (error) {
+                console.error('사용자 정보 가져오기 오류:', error);
+                res.status(500).json({ error: '내부 서버 오류' });
+            } else {
+                if (results.length > 0) {
+                    const userInfo = {
+                        id: results[0].id,
+                        name: results[0].name,
+                        phone: results[0].phone
+                    };
+                    res.json(userInfo);
+                } else {
+                    res.status(404).json({ error: '사용자 정보를 찾을 수 없음' });
+                }
+            }
+        });
+    } else {
+        res.status(401).json({ error: '로그인되지 않은 사용자' });
+    }
+});
+
+app.post('/update', (req, res) => {
+    const userData = req.body;
+
+    const query = 'UPDATE user SET password = ?, name = ?, phone = ? WHERE id = ?';
+    const values = [userData.password, userData.name, userData.phone, userData.id];
+
+    connection.query(query, values, (error, results, fields) => {
+        if (error) {
+            console.error('회원정보 수정 오류:', error);
+            res.status(500).json({ success: false, message: '내부 서버 오류' });
+        } else {
+            console.log('회원정보 수정 성공');
+            res.status(200).json({ success: true, message: '회원정보 수정 성공' });
         }
     });
 });
